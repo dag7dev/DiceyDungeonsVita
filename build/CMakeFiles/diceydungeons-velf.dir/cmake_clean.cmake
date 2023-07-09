38b2d3b36417a8d582239733ae5d5c7bf3170502
dag7dev <dag7@protonmail.com>
2023-07-09 20:14:15 +0200

    removed build

--- build/CMakeFiles/diceydungeons-velf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/diceydungeons-velf"
-  "diceydungeons.velf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/diceydungeons-velf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/diceydungeons.vpk-vpk.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for diceydungeons.vpk-vpk.
-# This may be replaced when dependencies are built.
